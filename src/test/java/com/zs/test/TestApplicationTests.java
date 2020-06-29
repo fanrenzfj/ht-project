210643429331e2932b9d737ee2a5a1a3bd38bafe
zhengshuai <fanrenzfj@126.com>
2020-06-29 15:25:07 +0800

    00

---
+++ src/test/java/com/zs/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zs.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
